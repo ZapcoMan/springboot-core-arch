1c3f53402b611850493def14706d3a54265aa52f
codervibe <3217277135@qq.com>
2025-08-04 16:39:29 +0800

    feat(log): 添加审计日志服务实现类
    - 实现了审计日志的保存、查询和删除功能
    - 提供了获取最近审计日志的方法
    - 继承了基础服务类，实现了通用的 CRUD 操作

---
+++ springboot-core-arch-server/common-log/src/main/java/org/example/log/service/impl/AuditLogServiceImpl.java
@@ -0,0 +1,115 @@
+package org.example.log.service.impl;
+
+
+import jakarta.annotation.Resource;
+import org.example.core.service.impl.BaseServiceImpl;
+import org.example.entity.AuditLog;
+import org.example.log.mapper.AuditLogMapper;
+import org.example.log.service.AuditLogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 审计日志服务实现类
+ * 该类负责处理审计日志的相关业务逻辑，如保存日志和获取最近的日志记录
+ */
+@Service
+public class AuditLogServiceImpl extends BaseServiceImpl<AuditLog, Long, AuditLogMapper> implements AuditLogService {
+
+    /**
+     * 审计日志数据访问对象
+     * 用于执行与审计日志相关的数据库操作
+     */
+    @Resource
+    private AuditLogMapper auditLogMapper;
+
+    public AuditLogServiceImpl(AuditLogMapper mapper) {
+        super(mapper);
+    }
+
+    /**
+     * 保存审计日志
+     *
+     * @param log 审计日志对象，包含日志的所有必要信息
+     * 将日志对象插入到数据库中，以便跟踪和审计系统操作
+     */
+    @Override
+    public void saveLog(AuditLog log) {
+        auditLogMapper.insert(log);
+    }
+
+    /**
+     * 获取最近的审计日志
+     *
+     * @param limit 限制获取的日志记录数量
+     * @return 最近的审计日志列表，按时间降序排列
+     * 该方法用于获取系统最近的操作日志，帮助管理员监控和审计系统活动
+     */
+    @Override
+    public List<AuditLog> getRecentLogs(int limit) {
+        return auditLogMapper.selectRecent(limit);
+    }
+
+    /**
+     * 根据ID查询审计日志信息
+     *
+     * @param id 审计日志ID
+     * @return AuditLog 审计日志信息
+     */
+    @Override
+    public AuditLog selectById(Long id) {
+        return auditLogMapper.selectById(id);
+    }
+
+
+    /**
+     * 查询所有审计日志信息
+     *
+     * @return List<AuditLog> 审计日志信息列表
+     */
+    @Override
+    public List<AuditLog> selectAll() {
+        return List.of();
+    }
+
+    /**
+     * 根据ID更新审计日志信息
+     *
+     * @param id 审计日志ID
+     */
+    @Override
+    public void updateById(Long id) {
+        auditLogMapper.updateById(id);
+    }
+
+    /**
+     * 插入审计日志信息
+     *
+     * @param auditLog 审计日志信息
+     */
+    @Override
+    public void insert(AuditLog auditLog) {
+
+    }
+
+    /**
+     * 根据ID更新审计日志信息
+     *
+     * @param auditLog 审计日志信息
+     */
+    @Override
+    public void updateById(AuditLog auditLog) {
+
+    }
+
+    /**
+     * 根据ID删除审计日志信息
+     *
+     * @param id 审计日志ID
+     */
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
