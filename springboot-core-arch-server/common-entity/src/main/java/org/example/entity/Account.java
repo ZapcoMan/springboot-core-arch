06961df3cade92508f00d7af202c3b7887f3e48f
codervibe <3217277135@qq.com>
2025-08-05 23:57:11 +0800

    refactor(entity): 为 Account 类添加 Lombok 注解
    - 在 Account 类中添加了 @AllArgsConstructor、@Data 和 @NoArgsConstructor 注解- 这些注解能够自动生成 getter、setter、构造函数等方法，简化代码并提高开发效率

--- springboot-core-arch-server/common-entity/src/main/java/org/example/entity/Account.java
+++ springboot-core-arch-server/common-entity/src/main/java/org/example/entity/Account.java
@@ -1,6 +1,10 @@
 package org.example.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Account类代表系统中的用户账户信息
  * 它封装了用户相关的各种属性，如用户名、密码、角色等
